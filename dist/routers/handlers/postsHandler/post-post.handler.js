4f2a2a2255b167de4a34f8304f69a6c3a07a2325
Alexander Gerhard <870717ag@gmail.com>
2025-06-28 22:22:08 +0200

    Fix: blog existence check before post creation

--- dist/routers/handlers/postsHandler/post-post.handler.js
+++ dist/routers/handlers/postsHandler/post-post.handler.js
@@ -13,23 +13,31 @@ exports.createPostHandler = createPostHandler;
 const http_statuses_1 = require("../../../core/types/http-statuses");
 const posts_repository_1 = require("../../../posts/repositories/posts.repository");
 const map_to_post_view_model_util_1 = require("../../../posts/mappers/map-to-post-view-model.util");
+const blogs_repository_1 = require("../../../blogs/repositories/blogs.repository");
 function createPostHandler(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            const blogId = req.body.blogId;
+            // Проверяем, существует ли блог
+            const blog = yield blogs_repository_1.blogsRepository.findById(blogId);
+            if (!blog) {
+                res.sendStatus(http_statuses_1.HttpStatus.NotFound); // 404 если блог не найден
+                return;
+            }
             const newPost = {
                 title: req.body.title,
                 shortDescription: req.body.shortDescription,
                 content: req.body.content,
-                blogId: req.body.blogId,
-                blogName: "TemporaryBlogName",
+                blogId: blog._id.toString(),
+                blogName: blog.name, // используем настоящее имя блога
                 createdAt: new Date().toISOString(),
             };
             const createdPost = yield posts_repository_1.postsRepository.create(newPost);
             const postViewModel = (0, map_to_post_view_model_util_1.mapToPostViewModel)(createdPost);
-            res.status(http_statuses_1.HttpStatus.Created).send(postViewModel);
+            res.status(http_statuses_1.HttpStatus.Created).send(postViewModel); // 201 Created
         }
         catch (e) {
-            res.sendStatus(http_statuses_1.HttpStatus.InternalServerError);
+            res.sendStatus(http_statuses_1.HttpStatus.InternalServerError); // 500
         }
     });
 }
