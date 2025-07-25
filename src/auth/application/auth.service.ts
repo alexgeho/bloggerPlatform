50db37a2d1c6625260275683cad400325fcd9a91
Alexander Gerhard <870717ag@gmail.com>
2025-07-25 15:00:52 +0200

    adding JWT - part 4

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -4,6 +4,7 @@ import { Result } from '../common/result/result.type';
 import { jwtService } from '../adapters/jwt.service';
 import { WithId } from 'mongodb';
 import { IUserDB } from '../types/user.db.interface';
+import {userRepository} from "../../users/repositories/user.repository";
 
 export const authService = {
     async checkCredentials(
@@ -16,15 +17,15 @@ export const authService = {
 
         if (result.status !== ResultStatus.Success) {
             return {
-                status: result.status, // например: NotFound, BadRequest
-                errorMessage: result.errorMessage ?? 'Login failed',
-                extensions: result.extensions ?? [],
-                data: null,
+                status: ResultStatus.Unauthorized,
+                errorMessage: 'Unauthorized',
+                extensions: [{ field: 'loginOrEmail', message: 'Wrong credentials'}],
+                data: null
             };
         }
 
-        const user: WithId<IUserDB> = result.data!;
-        const accessToken = await jwtService.createToken(user._id.toString());
+
+        const accessToken = await jwtService.createToken(result.data!._id.toString());
 
         return {
             status: ResultStatus.Success,
@@ -32,4 +33,36 @@ export const authService = {
             extensions: [],
         };
     },
+
+    async checkUserCredentials(
+        loginOrEmail: string,
+        password: string,
+    ): Promise<Result<WithId<IUserDB> | null>> {
+
+        const user = await userRepository.findByLoginOrEmail(loginOrEmail);
+        if (!user)
+            return {
+                status: ResultStatus.NotFound,
+                data: null,
+                errorMessage: 'Not Found',
+                extensions: [{ field: 'loginOrEmail', message: 'Not Found' }],
+            };
+
+        const isPassCorrect = await bcryptService.checkPassword(password, user.passwordHash);
+        if (!isPassCorrect)
+            return {
+                status: ResultStatus.BadRequest,
+                data: null,
+                errorMessage: 'Bad Request',
+                extensions: [{ field: 'password', message: 'Wrong password' }],
+            };
+
+        return {
+            status: ResultStatus.Success,
+            data: user,
+            extensions: [],
+        };
+    },
+
+
 };
