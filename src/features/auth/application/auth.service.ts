7fee1d7c87674f762ba45e44608a49ee06355dce
alexgeho <870717ag@gmail.com>
2025-07-29 08:48:37 +0200

    Updated Users passwordValidation - 4

--- src/features/auth/application/auth.service.ts
+++ src/features/auth/application/auth.service.ts
@@ -20,7 +20,7 @@ export const authService = {
 return {
     status: ResultStatus.Unauthorized,
     errorMessage: 'Unauthorized',
-    extensions: [{ field: 'loginOrEmail', message: 'Wrong credentials bitau'}],
+    extensions: [{ field: 'loginOrEmail', message: 'Wrong credentials'}],
     data: null,
 };
 
