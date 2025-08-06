fa31c1d5c005b0de783966976c96986758a9a004
Alexander Gerhard <870717ag@gmail.com>
2025-08-06 18:50:33 +0200

    fix(auth): link in email.template to front, part 3, fixed ``

--- src/features/auth/adapters/email.templates.ts
+++ src/features/auth/adapters/email.templates.ts
@@ -1,10 +1,11 @@
 export const templates = {
     confirmation: (code: string): string => {
         return `
-      <h1>Thank for your registration</h1>
-      <p>To finish registration please follow the link below:
-<a href='https://alexgeho.github.io/bloggerPlatform-front/email-confirmed?code=${code}'>
-      </p>
-    `;
-    },
+  <h1>Thank you for your registration</h1>
+  <p>To finish registration please follow the link below:
+    <a href='https://alexgeho.github.io/bloggerPlatform-front/email-confirmed?code=${code}'>complete registration</a>
+  </p>
+        `;
+    }
 };
+
