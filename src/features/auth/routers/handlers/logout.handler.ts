599e3b276153766b3c817ad88df7b14b2481cb85
Alexander Gerhard <870717ag@gmail.com>
2025-08-14 04:10:50 +0200

    fix: logoutHandler, checking refreshToken

--- src/features/auth/routers/handlers/logout.handler.ts
+++ src/features/auth/routers/handlers/logout.handler.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from 'express';
 import { refreshCookieOptions } from '../../../../core/http/cookie';
+import {jwtService} from "../../adapters/jwt.service";
 
-export async function logoutHandler(_req: Request, res: Response) {
-    // опционально: здесь можно инвалидировать refresh в БД
-    res.clearCookie('refreshToken', { ...refreshCookieOptions, maxAge: 0 });
-    res.sendStatus(204);
+export async function logoutHandler(
+    req: Request,
+    res: Response
+): Promise<void>
+{
+    const refreshToken = req.cookies?.refreshToken;
+
+    if (!refreshToken) {
+         res.sendStatus(401);
+        return// токена нет
+    }
+
+    const payload = await jwtService.verifyRefreshToken(refreshToken);
+    if (!payload) {
+         res.sendStatus(401);
+        return// токен просрочен или сломан
+    }
+
+    res.clearCookie('refreshToken', refreshCookieOptions);
+     res.sendStatus(204);
+    return
 }
