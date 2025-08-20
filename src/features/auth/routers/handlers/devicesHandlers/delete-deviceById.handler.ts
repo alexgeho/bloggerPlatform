dc2e909dfe9a6f40ec011fc9fa5ca9b2b85802b7
Alexander Gerhard <870717ag@gmail.com>
2025-08-20 16:05:01 +0200

    fix: router for devices in Object style, part 2

---
+++ src/features/auth/routers/handlers/devicesHandlers/delete-deviceById.handler.ts
@@ -0,0 +1,15 @@
+import { Request, Response} from 'express';
+import { HttpStatus} from "../../../../../core/types/http-statuses";
+import { errorsHandler } from '../../../../../core/errors/errors.handler';
+
+export async function deleteDeviceByIdHandler(
+    req: Request <{id: string}>,
+    res: Response) {
+
+
+
+
+
+}
+
+
